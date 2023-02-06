b4800a949d7ee08221f362dbe5fb7f749879db44
jj <santiago66jm@gmail.com>
2023-02-05 22:24:09 -0500

    url httpssxx

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
         if(config(key:'app.env')==='production'){
             URL::forceScheme(sheme:'https');
         }
